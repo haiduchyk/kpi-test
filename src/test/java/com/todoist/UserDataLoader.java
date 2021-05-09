4aee201222b3037efdaa0052815543e5879fe84d
tymur.haiduchyk <haiduchyk@gmail.com>
2021-05-09 15:50:57 +0300

    Add data for tests

--- src/test/java/com/todoist/UserDataLoader.java
+++ src/test/java/com/todoist/UserDataLoader.java
@@ -7,8 +7,8 @@ public class UserDataLoader
     public TestUser Load()
     {
         Map<String, String> env = System.getenv();
-        String email = env.get("EMAIL");
-        String password = env.get("PASSWORD");
+        String email = "haiduchyk.t@gmail.com";
+        String password = "qwe12345678";
         TestUser testUser = new TestUser();
         testUser.email = email;
         testUser.password = password;
